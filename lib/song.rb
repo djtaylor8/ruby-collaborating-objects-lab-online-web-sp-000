d32037e7dddce483636bb972a716c1a130114913
DJ Taylor <dtaylor88@gmail.com>
2021-06-09 02:02:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
   end
   
   def self.new_by_filename(filename)
-    song = self.new(name)
+    song = self.new(filename)
     song.name = filename.split(" - ")[1]
     song.artist = filename.split(" - ")[0]
     song 
